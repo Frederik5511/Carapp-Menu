9dc1cd23a5c29e7063e76aa0952240a4ae4f9ec5
Frederik <FrederikPedersen55@outlook.com>
2026-02-19 11:55:28 +0100

    Tilføjer skelet af menuen

--- Carapp Menu/Program.cs
+++ Carapp Menu/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace Carapp_Menu
 {
     internal class Program
-    {
+    {   
+
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("\nMENU");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
         }
     }
