b2b49895f33e546c3c28696f9071fdc4e5e1d5eb
Frederik <FrederikPedersen55@outlook.com>
2026-03-03 12:31:30 +0100

    Opretter Car.cs og flytter metoder over

---
+++ Carapp Menu/Car.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Carapp_Menu
+{
+    internal class Car
+    {
+        private string _brand = "";
+        private string _model = "";
+        private int _year = 0;
+        private int _mileage = 0;
+        private bool _IsEngineOn = false;
+        private double _kmPerLiter = 0;
+        public void ReadCarDetails()
+        {
+            Console.WriteLine("Hvilket bilmærke er din bil?");
+            _brand = Console.ReadLine();
+            Console.WriteLine("Hvilken model er din bil?");
+            _model = Console.ReadLine();
+            Console.WriteLine("Hvilken Årgang er din bil fra?");
+
+            // while betyder: kør så længe betingelsen er sand
+            // int.TryParse prøver at lave brugerens tekst om til et helt tal (int)
+            // out year betyder: hvis det lykkes, gem tallet i variablen year
+            // ! betyder "ikke"
+            // Så loopet kører så længe input IKKE kan konverteres til et tal
+            while (!int.TryParse(Console.ReadLine(), out _year))
+            {
+                // Hvis brugeren skriver noget der ikke er et tal,
+                // kommer vi herind og beder dem prøve igen
+                Console.WriteLine("Ugyldigt svar. Prøv igen");
+            }
+
+            Console.WriteLine("Hvad er din kilometerstand?");
+            // Samme som tidligere bare med mileage i stedet for year
+            while (!int.TryParse(Console.ReadLine(), out _mileage))
+            {
+                Console.WriteLine("Ugyldigt svar. Prøv igen");
+            }
+            Console.WriteLine("Hvor langt kører din bil per liter?");
+            while (!double.TryParse(Console.ReadLine(), out _kmPerLiter))
+            {
+                Console.WriteLine("Ugyldigt svar. Prøv igen");
+            }
+
+
+
+        }
+
+        public void StartEngine()
+        {
+            _IsEngineOn = true;
+            Console.WriteLine("Motoren er nu tændt");
+        }
+
+        public void StopEngine()
+        {
+            _IsEngineOn = false;
+            Console.WriteLine("Motoren er nu slukket");
+        }
+
+        public void Drive(double distance)
+        {
+            if (_IsEngineOn)
+            {
+                _mileage += (int)Math.Round(distance);
+                Console.WriteLine($"Ny kilometerstand: {_mileage}");
+            }
+            else
+            {
+                Console.WriteLine("Tænd motoren");
+            }
+        }
+
+        public double CalculateTripPrice(double distance, double LiterPrice, string FuelType)
+        {
+            // Undgå division med 0
+            if (_kmPerLiter == 0)
+            {
+                Console.WriteLine("Fejl: kmPerLiter er 0. Indtastbilinfo først(punkt 1)");
+                return 0;
+            }
+            // Tjek brændstoftype
+            FuelType = (FuelType ?? "").Trim().ToLower();
+            if (FuelType != "benzin" && FuelType != "diesel")
+            {
+                Console.WriteLine("Fejl: Brændstoftype skal være 'benzin' eller 'diesel'");
+                return 0;
+            }
+            // Beregn forbrug og pris
+            double LitersUsed = distance / _kmPerLiter;
+            double TotalPrice = LitersUsed * LiterPrice;
+            return TotalPrice;
+        }
+        public bool IsPalindrome(int km)
+        {
+            // Lav tallet om til tekst
+            string value = km.ToString();
+
+            int left = 0;
+            int right = value.Length - 1;
+
+            // Sammenlign yderste tegn og arbejd indad
+            while (left < right)
+            {
+                if (value[left] != value[right])
+                {
+                    return false; //Hvis noget ikke matcher, er det ikke et palindrom
+                }
+                left++; // Flytter én position ind fra venstre
+                right--; // Flytter én position ind fra højre
+            }
+            // Hvis vi når hertil uden mismatch, er det et palindrom
+            return true;
+        }
+        public void PrintCarDetails()
+        {
+
+        }
+    }
+}
